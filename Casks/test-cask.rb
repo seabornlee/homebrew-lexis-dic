b51739c2cad676b82a51613af170557d83edeafb
Water Lee <waterlee.site@gmail.com>
2026-04-23 21:54:57 +0800

    Add test cask

---
+++ Casks/test-cask.rb
@@ -0,0 +1,10 @@
+class TestCask < Cask
+  version "1.0.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  url "https://example.com/test.dmg"
+  name "Test Cask"
+  homepage "https://example.com"
+
+  app "Test.app"
+end
