592f50fd45593abb14a078c2e64303948988913f
Water Lee <waterlee.site@gmail.com>
2026-04-23 21:28:48 +0800

    Use Cask instead of Formula

--- Formula/lexis-dic.rb
+++ Formula/lexis-dic.rb
@@ -1,6 +1,4 @@
-class LexisDic < Formula
-  desc "AI-powered English Dictionary for macOS"
-  homepage "https://github.com/seabornlee/ai-dic-repos"
+class LexisDic < Cask
   version "1.0.19"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
@@ -8,17 +6,11 @@ class LexisDic < Formula
   name "LexisDic"
   desc "AI-powered English Dictionary for macOS"
   homepage "https://github.com/seabornlee/ai-dic-repos"
-  license "MIT"
 
-  def install
-    require "json"
-    plist = `/usr/bin/hdiutil attach -nobrowse -plist "#{cached_download}" 2>/dev/null`.chomp
-    vol = JSON.parse(plist)["system-entities"].find { |e| e["mount-point"] }["mount-point"]
-    FileUtils.cp_r "#{vol}/LexisDic.app", "/Applications/LexisDic.app"
-    system "hdiutil", "detach", vol, force: true
-  end
+  app "LexisDic.app"
 
-  test do
-    assert_predicate File.exist?("/Applications/LexisDic.app"), "App should be installed"
-  end
+  zap trash: [
+    "~/Library/Application Support/LexisDic",
+    "~/Library/Preferences/site.waterlee.aidic.LexisDic.plist",
+  ]
 end
